b189c4bf3d6775522463c769688c23c5135c5dc8
SakanaDev <j.peig18@gmail.com>
2025-03-15 16:35:47 +0100

    map working

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,6 +5,8 @@
 //     document.getElementById(tabId).classList.add('active');
 // }
 // Funció per amagar o mostrar pestanyes
+window.onload = () => document.getElementById('maptab').style.display = 'none';
+
 function showTab(tabId, display) {
     document.querySelectorAll('.tab').forEach(tab => {
         tab.style.display = 'none';
