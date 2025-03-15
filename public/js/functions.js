4b13f6ac0f126a4d7a81e6d7a4264d4cd0b2a3ad
SakanaDev <j.peig18@gmail.com>
2025-03-15 16:22:14 +0100

    test Tiempo widget2

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,9 +1,5 @@
-async function showWeather() {
-    const content = document.getElementById('weather');
-
-    let data = await fetch('/tiempo');
-    let dataJson = await data.json();
-    
-    console.log(dataJson);
-    
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const weather = document.getElementById('weather');
+    weather.innerHTML = 'test';
+    alert('test')
+})
