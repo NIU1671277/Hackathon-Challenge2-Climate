18844595bc1078bf2a0fdcf185f9cd51815ca779
SakanaDev <j.peig18@gmail.com>
2025-03-15 15:27:35 +0100

    test Tiempo widget

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/tiempo', (req, res) => {
+    data = path.join(__dirname, 'misc/data_current_weather.js');
+    res.sendFile(data);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
